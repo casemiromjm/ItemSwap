953e2ee686094e369cc6371cab46155fe5f81f95
palopao <up202307047@up.pt>
2025-03-23 00:11:42 +0000

    updated prototype

--- prototype/screens/signup_screen.dart
+++ prototype/screens/signup_screen.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'image_handler.dart';
+import 'login_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -12,36 +10,16 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  final TextEditingController _emailController =
-      TextEditingController(); // Email controller
-  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  String? _imageBase64;
-  bool _isPasswordVisible = false; // Track password visibility
-  bool _isLoading = false; // Track the loading state
+  bool _isPasswordVisible = false;
+  bool _isLoading = false;
   String _errorMessage = '';
 
-  // Firebase Auth instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Firebase Firestore instance
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // Function to pick image
-  Future<void> _pickImage() async {
-    await pickImage((compressedImageBase64) {
-      setState(() {
-        _imageBase64 = compressedImageBase64;
-      });
-    });
-  }
-
-  // Function to send verification email and create user if verified
   Future<void> _signUp() async {
-    if (_usernameController.text.isEmpty ||
-        _passwordController.text.isEmpty ||
-        _imageBase64 == null ||
-        _emailController.text.isEmpty) {
+    if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Please fill in all fields')),
       );
@@ -50,18 +28,16 @@ class _SignupScreenState extends State<SignupScreen> {
 
     setState(() {
       _isLoading = true;
-      _errorMessage = ''; // Reset error message
+      _errorMessage = '';
     });
 
     try {
-      // Create user using FirebaseAuth with email
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(
-            email: _emailController.text, // Use the email entered by the user
+            email: _emailController.text,
             password: _passwordController.text,
           );
 
-      // Send verification email
       await userCredential.user!.sendEmailVerification();
 
       ScaffoldMessenger.of(context).showSnackBar(
@@ -70,12 +46,10 @@ class _SignupScreenState extends State<SignupScreen> {
         ),
       );
 
-      // After sending email, allow user to check verification status manually
       setState(() {
         _isLoading = false;
       });
 
-      // You can guide the user here to check their email and manually refresh
       _showVerificationDialog(userCredential.user!);
     } on FirebaseAuthException catch (e) {
       setState(() {
@@ -85,7 +59,6 @@ class _SignupScreenState extends State<SignupScreen> {
     }
   }
 
-  // Show a dialog asking the user to check their email and refresh the status
   void _showVerificationDialog(User user) {
     showDialog(
       context: context,
@@ -99,14 +72,19 @@ class _SignupScreenState extends State<SignupScreen> {
               const SizedBox(height: 10),
               ElevatedButton(
                 onPressed: () async {
-                  // Reload the user and check email verification status
                   await user.reload();
                   User? updatedUser = _auth.currentUser;
 
-                  // Make sure the user is reloaded correctly
                   if (updatedUser != null && updatedUser.emailVerified) {
                     Navigator.of(context).pop();
-                    _createUserInFirestore(updatedUser);
+                    // Proceed with profile creation or other logic
+                    // Redirect user to login screen for further steps
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginScreen(),
+                      ),
+                    );
                   } else {
                     setState(() {
                       _errorMessage =
@@ -127,24 +105,6 @@ class _SignupScreenState extends State<SignupScreen> {
     );
   }
 
-  // Function to save user to Firestore
-  Future<void> _createUserInFirestore(User user) async {
-    String userId = user.uid;
-
-    // Save user data to Firestore, including userCreatedCount
-    await _firestore.collection('users').doc(userId).set({
-      'username': _usernameController.text,
-      'image': _imageBase64,
-      'created_at': Timestamp.now(),
-      'items_given': 0,
-      'items_received': 0,
-    });
-
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(const SnackBar(content: Text('User created successfully!')));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -153,91 +113,73 @@ class _SignupScreenState extends State<SignupScreen> {
         backgroundColor: const Color.fromARGB(255, 63, 133, 190),
         title: const Text('Sign Up', style: TextStyle(color: Colors.white)),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: _pickImage,
-                  child: CircleAvatar(
-                    radius: 100,
-                    backgroundColor: Colors.grey,
-                    backgroundImage:
-                        _imageBase64 != null
-                            ? MemoryImage(base64Decode(_imageBase64!))
-                            : null,
-                    child:
-                        _imageBase64 == null
-                            ? const Icon(
-                              Icons.camera_alt,
-                              color: Colors.white,
-                              size: 40,
-                            )
-                            : null,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _emailController, // Email input field
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: const TextStyle(color: Colors.white),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  maxLength: 15,
-                  controller: _usernameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: const TextStyle(color: Colors.white),
-                ),
-                const SizedBox(height: 16),
-                TextField(
-                  maxLength: 15,
-                  controller: _passwordController,
-                  obscureText:
-                      !_isPasswordVisible, // Toggle password visibility
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: const TextStyle(color: Colors.white),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isPasswordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off,
-                        color: Colors.white,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          controller: _emailController,
+                          maxLength: 300,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible;
-                        });
-                      },
-                    ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          maxLength: 15,
+                          controller: _passwordController,
+                          obscureText: !_isPasswordVisible,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: const TextStyle(color: Colors.white),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _isPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isPasswordVisible = !_isPasswordVisible;
+                                });
+                              },
+                            ),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: _isLoading ? null : _signUp,
+                        child:
+                            _isLoading
+                                ? const CircularProgressIndicator()
+                                : const Text('Sign Up'),
+                      ),
+                    ],
                   ),
-                  style: const TextStyle(color: Colors.white),
                 ),
-                const SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed:
-                      _isLoading
-                          ? null
-                          : _signUp, // Disable button while loading
-                  child:
-                      _isLoading
-                          ? const CircularProgressIndicator()
-                          : const Text('Sign Up'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
