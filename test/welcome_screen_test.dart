4169960489592a09444039bf1af1bfef639b864c
casemiromjm <casemiromelojorge@gmail.com>
2025-05-29 12:15:02 +0100

    done welcome tests

--- test/welcome_screen_test.dart
+++ test/welcome_screen_test.dart
@@ -1,38 +1,68 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:itemswap/screens/welcome_screen.dart';
 import 'package:itemswap/screens/login_screen.dart';
 import 'package:itemswap/screens/signup_screen.dart';
+import 'mocks/auth_mock.dart';
 
 void main() {
-  testWidgets('WelcomeScreen renders correctly and navigates', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: WelcomeScreen(),
-      ),
-    );
+  // IMPORTANT: Set up the Firebase Core mocks at the very beginning.
+  setupFirebaseAuthMocks();
 
-    // Verifica se a imagem do logo é exibida
-    expect(find.byType(Image), findsOneWidget);
+  // Initialize Firebase before tests run.
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
 
-    // Verifica se os botões Login e Sign Up estão presentes
-    expect(find.widgetWithText(ElevatedButton, 'Login'), findsOneWidget);
-    expect(find.widgetWithText(ElevatedButton, 'Sign Up'), findsOneWidget);
+  group('WelcomeScreen tests', () {
+    testWidgets('WelcomeScreen renders correctly', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: WelcomeScreen(),
+        ),
+      );
 
-    // Clica no botão Login e verifica se navega para LoginScreen
-    /*await tester.tap(find.widgetWithText(ElevatedButton, 'Login'));
-    await tester.pumpAndSettle(); // espera pela navegação
+      // Verifica se a imagem do logo é exibida
+      expect(find.byType(Image), findsOneWidget);
 
-    expect(find.byType(LoginScreen), findsOneWidget);
+      // Verifica se os botões Login e Sign Up estão presentes
+      expect(find.widgetWithText(ElevatedButton, 'Login'), findsOneWidget);
+      expect(find.widgetWithText(ElevatedButton, 'Sign Up'), findsOneWidget);
+    });
 
-    // Volta para WelcomeScreen
-    await tester.pageBack();
-    await tester.pumpAndSettle();
+    testWidgets(
+        'WelcomeScreen navigates to Login screen', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: WelcomeScreen(),
+        ),
+      );
 
-    // Clica no botão Sign Up e verifica se navega para SignupScreen
-    await tester.tap(find.widgetWithText(ElevatedButton, 'Sign Up'));
-    await tester.pumpAndSettle(); // espera pela navegação
+      // Clica no botão Login e verifica se navega para LoginScreen
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Login'));
+      await tester.pumpAndSettle(); // espera pela navegação
 
-    expect(find.byType(SignupScreen), findsOneWidget);*/
+      expect(find.byType(LoginScreen), findsOneWidget);
+
+      // Volta para WelcomeScreen
+      await tester.pageBack();
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('WelcomeScreen navigates to Sign Up screen', (
+        WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: WelcomeScreen(),
+        ),
+      );
+
+      // Clica no botão Sign Up e verifica se navega para SignupScreen
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Sign Up'));
+      await tester.pumpAndSettle(); // espera pela navegação
+
+      expect(find.byType(SignupScreen), findsOneWidget);
+    });
   });
 }
